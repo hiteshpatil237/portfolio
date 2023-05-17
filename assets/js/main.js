3c96f41814a744ba7d9347cb119204d16d0514de
hiteshpatil237 <hiteshpatil237@gmail.com>
2023-05-17 12:27:48 -0400

    Applied few changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -108,27 +108,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
 });
 
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop: true,
-    grabCursor: true,
-    spaceBetween: 48,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-    },
-    breakpoints: {
-        568: {
-            slidesPerView: 2,
-        }
-    }
-});
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
